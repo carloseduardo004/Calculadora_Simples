108c34959de15ef2237d0fd755e21824034a9330
carloseduardo004 <carlosedumachado00@gmail.com>
2023-06-28 21:31:48 -0300

    Definição da lógica de negócio

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+ope= {"+": function(x,y){return x+y},
+    "-": function(x,y){return x-y},
+    "*": function(x,y){return x*y},
+    "÷": function(x,y){return x/y}
+}
+
 function calcular(){
-let num1 = document.querySelector("#num1").value;
-let num2 = document.querySelector("#num2").value;
+let num1 = parseFloat(document.querySelector("#num1").value);
+let num2 = parseFloat(document.querySelector("#num2").value);
 let op = document.querySelector("#select").value;
-let result = document.querySelector("#result").innerText;
+document.querySelector("#result").innerText = (num1+" "+op+" "+num2+" = " + ope[op](num1,num2))
 }
