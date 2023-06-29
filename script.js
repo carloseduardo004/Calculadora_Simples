31eb1b0b60d2dd07f58b671c0ebb542f0dfe7ece
carloseduardo004 <carlosedumachado00@gmail.com>
2023-06-28 21:05:49 -0300

    Definindo a função, números, operador e resultado

---
+++ script.js
@@ -0,0 +1,6 @@
+function calcular(){
+let num1 = document.querySelector("#num1").value;
+let num2 = document.querySelector("#num2").value;
+let op = document.querySelector("#select").value;
+let result = document.querySelector("#result").innerText;
+}
