dd624d1395eb2ad5b4bfc525a2ad9607d181fbc6
carloseduardo004 <carlosedumachado00@gmail.com>
2023-06-28 22:24:09 -0300

    Checagem de Inputs

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
+const form = document.getElementById("form");
+
 ope= {"+": function(x,y){return x+y},
     "-": function(x,y){return x-y},
     "*": function(x,y){return x*y},
     "÷": function(x,y){return x/y}
-}
+};
 
-function calcular(){
+
+form.addEventListener("submit",(e) =>{
+e.preventDefault()
 let num1 = parseFloat(document.querySelector("#num1").value);
 let num2 = parseFloat(document.querySelector("#num2").value);
 let op = document.querySelector("#select").value;
 document.querySelector("#result").innerText = (num1+" "+op+" "+num2+" = " + ope[op](num1,num2))
-}
+})
